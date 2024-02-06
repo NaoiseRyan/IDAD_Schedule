9fa3f2ae60460bd4b00c1271dd2fb891146cc735
Naoise Ryan <naoisethecool@gmail.com>
2024-02-06 16:32:50 +0000

    Added function to get current hour and check if class is on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,47 @@
 function main() {
     updateHTML();
-
+    console.log(getCurrentHour())
     setTimeout(main, 1000);
 }
 
-function updateHTML(){
+function updateHTML() {
     document.getElementById("currentDayId").textContent = getCurrentDay();
+    let inClassTimeRange = isClassTimeRange();
+    console.log(inClassTimeRange)
+}
+
+function isClassTimeRange() {
+    let currentHour = getCurrentHour();
+    let isWithinRange = false;
+    switch (getCurrentDay()) {
+        case "Monday":
+            if (currentHour >= 9 && currentHour <= 16) {
+                isWithinRange = true;
+            }
+            break;
+        case "Tuesday":
+            if (currentHour >= 9 && currentHour <= 14) {
+                isWithinRange = true;
+            }
+            break;
+        case "Wednesday":
+                isWithinRange = false;
+            break;
+        case "Thursday":
+            if (currentHour >= 9 && currentHour <= 12) {
+                isWithinRange = true;
+            }
+            break;
+        case "Friday":
+            if (currentHour >= 11 && currentHour <= 12) {
+                isWithinRange = true;
+            }
+            break;
+        default:
+            isWithinRange = false;
+            break;
+    }
+    return isWithinRange;
 }
 
 function getCurrentDay() {
@@ -36,6 +72,26 @@ function getCurrentDay() {
     }
     return currentDay
 }
+function getCurrentHour() {
+    let currentHour = new Date().getHours();
+    return currentHour
+}
 
+
+var timeTable =
+    [
+        [1, 1, 2, 2, 0, 3, 4, 1],
+        [2, 3, 2, 0, 4, 4, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 3, 3, 0, 0, 0, 0],
+        [0, 0, 3, 3, 0, 0, 0, 0]
+    ]
+/*
+    0 = No class 
+    1 = Intro to 3d Digital Art
+    2 = Ui Prototyping
+    3 = Web Ui Design
+    4 = Design Psycology
+*/
 
 window.onload = main();
